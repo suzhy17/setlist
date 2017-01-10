52e17d5eef8006ee4f63bb0cd883a530a63190e0
suzhy <suzhy@DAOU-suzhy>
2017-01-10 19:30:09 +0900

    테스트 코드 추가

---
+++ setlist-root/setlist-web/src/test/java/com/daou/setlist/web/domain/ArtistTest.java
@@ -0,0 +1,14 @@
+package com.daou.setlist.web.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ArtistTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
